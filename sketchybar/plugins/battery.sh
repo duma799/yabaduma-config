18956564d601d93a1ab6b56db8c5a7b131be6573
duma799 <gptduma@gmail.com>
2025-12-31 03:18:01 +0400

    Fix battery icons

--- sketchybar/plugins/battery.sh
+++ sketchybar/plugins/battery.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-# battery.sh - Battery Status Plugin for SketchyBar
-#
-# Description: Displays battery percentage with dynamic icon
-# Dependencies: None (uses built-in macOS pmset)
-# Update Frequency: Every 120 seconds
 
-# Get battery percentage with validation
 PERCENTAGE=$(pmset -g batt | grep -Eo '[0-9]+%' | head -1 | tr -d '%')
-PERCENTAGE=${PERCENTAGE:-0}  # Default to 0 if empty
+PERCENTAGE=${PERCENTAGE:-0}
 
 CHARGING=$(pmset -g batt | grep 'AC Power')
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON="󰁹"
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON="󰂀"
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON="󰁾"
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON="󰁻"
   ;;
-  *) ICON=""
+  *) ICON="󰁺"
 esac
 
 if [[ -n "$CHARGING" ]]; then
